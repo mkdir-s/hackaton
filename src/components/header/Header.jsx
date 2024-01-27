de20ab5ebc9fde749298ac9ee0d0835c0b102ec8
artem alexeenko <artyom.alexeyenko@gmail.com>
2024-01-27 14:40:59 +0600

    initial projece, create home page, header component, connect formik and yup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.scss';
+
+function Home() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Home
